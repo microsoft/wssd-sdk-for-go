77a22d97ea38aa99e95286120801071a14248f16
Madhan Raj Mookkandy <madhanm@microsoft.com>
2019-10-23 12:11:54 -0700

    Vendor

--- vendor/github.com/microsoft/wssdagent/rpc/client/client.go
+++ vendor/github.com/microsoft/wssdagent/rpc/client/client.go
@@ -4,6 +4,8 @@
 package client
 
 import (
+	"os"
+	"strings"
 	"fmt"
 	compute_pb "github.com/microsoft/wssdagent/rpc/compute"
 	security_pb "github.com/microsoft/wssdagent/rpc/security"
@@ -13,22 +15,50 @@ import (
 	log "k8s.io/klog"
 
 	"github.com/microsoft/wssdagent/pkg/apis/config"
+	"github.com/microsoft/wssd-sdk-for-go/pkg/auth"
 )
+
+const debugModeTLS = "WSSD_DEBUG_MODE"
+
+
+// Returns nil if debug mode is on; err if it is not
+func isDebugMode() error {
+	debugEnv := strings.ToLower(os.Getenv(debugModeTLS))
+	if debugEnv == "on" {
+		return nil
+	}
+	return fmt.Errorf("Debug Mode not set")
+}
 
 func getServerEndpoint(serverAddress *string) string {
 	return fmt.Sprintf("%s:%d", *serverAddress, config.ServerPort)
 }
 
-func getDefaultDialOption() []grpc.DialOption {
+func getDefaultDialOption(authorizer auth.Authorizer) []grpc.DialOption {
 	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithInsecure())
+
+	// Debug Mode allows us to talk to wssdagent without a proper handshake
+	// This means we can debug and test wssdagent without generating certs
+	// and having proper tokens
+
+	// Check if debug mode is on
+	if ok := isDebugMode(); ok == nil {
+		// Working on a way to have debug mode still include tokens ... but not include
+		// cert generation ... for now that is out of scope
+		// creds := credentials.NewTLS(&tls.Config{InsecureSkipVerify: true})
+		opts = append(opts, grpc.WithInsecure())
+	} else {
+
+		opts = append(opts, grpc.WithTransportCredentials(authorizer.WithTransportAuthorization()))
+		opts = append(opts, grpc.WithPerRPCCredentials(authorizer.WithRPCAuthorization()))
+	}
 
 	return opts
 }
 
 // GetVirtualNetworkClient returns the virtual network client to communicate with the wssdagent
-func GetVirtualNetworkClient(serverAddress *string) (network_pb.VirtualNetworkAgentClient, error) {
-	opts := getDefaultDialOption()
+func GetVirtualNetworkClient(serverAddress *string, authorizer auth.Authorizer) (network_pb.VirtualNetworkAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
 	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
 	if err != nil {
 		log.Fatalf("Unable to get VirtualNetworkClient. Failed to dial: %v", err)
@@ -38,8 +68,9 @@ func GetVirtualNetworkClient(serverAddress *string) (network_pb.VirtualNetworkAg
 }
 
 // GetVirtualNetworkInterfaceClient returns the virtual network interface client to communicate with the wssd agent
-func GetVirtualNetworkInterfaceClient(serverAddress *string) (network_pb.VirtualNetworkInterfaceAgentClient, error) {
-	opts := getDefaultDialOption()
+func GetVirtualNetworkInterfaceClient(serverAddress *string, authorizer auth.Authorizer) (network_pb.VirtualNetworkInterfaceAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
+
 	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
 	if err != nil {
 		log.Fatalf("Unable to get VirtualNetworkInterfaceClient. Failed to dial: %v", err)
@@ -49,8 +80,8 @@ func GetVirtualNetworkInterfaceClient(serverAddress *string) (network_pb.Virtual
 }
 
 // GetLoadBalancerClient returns the loadbalancer client to communicate with the wssd agent
-func GetLoadBalancerClient(serverAddress *string) (network_pb.LoadBalancerAgentClient, error) {
-	opts := getDefaultDialOption()
+func GetLoadBalancerClient(serverAddress *string, authorizer auth.Authorizer) (network_pb.LoadBalancerAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
 	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
 	if err != nil {
 		log.Fatalf("Unable to get LoadBalancerClient. Failed to dial: %v", err)
@@ -60,8 +91,8 @@ func GetLoadBalancerClient(serverAddress *string) (network_pb.LoadBalancerAgentC
 }
 
 // GetVirtualMachineClient returns the virtual machine client to comminicate with the wssd agent
-func GetVirtualMachineClient(serverAddress *string) (compute_pb.VirtualMachineAgentClient, error) {
-	opts := getDefaultDialOption()
+func GetVirtualMachineClient(serverAddress *string, authorizer auth.Authorizer) (compute_pb.VirtualMachineAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
 	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
 	if err != nil {
 		log.Fatalf("Unable to get VirtualMachineClient. Failed to dial: %v", err)
@@ -71,8 +102,8 @@ func GetVirtualMachineClient(serverAddress *string) (compute_pb.VirtualMachineAg
 }
 
 // GetVirtualMachineScaleSetClient returns the virtual machine client to comminicate with the wssd agent
-func GetVirtualMachineScaleSetClient(serverAddress *string) (compute_pb.VirtualMachineScaleSetAgentClient, error) {
-	opts := getDefaultDialOption()
+func GetVirtualMachineScaleSetClient(serverAddress *string, authorizer auth.Authorizer) (compute_pb.VirtualMachineScaleSetAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
 	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
 	if err != nil {
 		log.Fatalf("Unable to get VirtualMachineScaleSetClient. Failed to dial: %v", err)
@@ -82,8 +113,8 @@ func GetVirtualMachineScaleSetClient(serverAddress *string) (compute_pb.VirtualM
 }
 
 // GetVirtualHardDiskClient returns the virtual network client to communicate with the wssdagent
-func GetVirtualHardDiskClient(serverAddress *string) (storage_pb.VirtualHardDiskAgentClient, error) {
-	opts := getDefaultDialOption()
+func GetVirtualHardDiskClient(serverAddress *string, authorizer auth.Authorizer) (storage_pb.VirtualHardDiskAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
 	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
 	if err != nil {
 		log.Fatalf("Unable to get VirtualHardDiskClient. Failed to dial: %v", err)
@@ -93,8 +124,8 @@ func GetVirtualHardDiskClient(serverAddress *string) (storage_pb.VirtualHardDisk
 }
 
 // GetKeyVaultClient returns the keyvault client to communicate with the wssdagent
-func GetKeyVaultClient(serverAddress *string) (security_pb.KeyVaultAgentClient, error) {
-	opts := getDefaultDialOption()
+func GetKeyVaultClient(serverAddress *string, authorizer auth.Authorizer) (security_pb.KeyVaultAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
 	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
 	if err != nil {
 		log.Fatalf("Unable to get KeyVaultClient. Failed to dial: %v", err)
@@ -104,12 +135,34 @@ func GetKeyVaultClient(serverAddress *string) (security_pb.KeyVaultAgentClient, 
 }
 
 // GetSecretClient returns the secret client to communicate with the wssdagent
-func GetSecretClient(serverAddress *string) (security_pb.SecretAgentClient, error) {
-	opts := getDefaultDialOption()
+func GetSecretClient(serverAddress *string, authorizer auth.Authorizer) (security_pb.SecretAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
 	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
 	if err != nil {
 		log.Fatalf("Unable to get KeyVaultClient. Failed to dial: %v", err)
 	}
 
 	return security_pb.NewSecretAgentClient(conn), nil
+}
+
+// GetIdentityClient returns the secret client to communicate with the wssdagent
+func GetIdentityClient(serverAddress *string, authorizer auth.Authorizer) (security_pb.IdentityAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
+	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
+	if err != nil {
+		log.Fatalf("Unable to get IdentityClient. Failed to dial: %v", err)
+	}
+
+	return security_pb.NewIdentityAgentClient(conn), nil
+}
+
+// GetAuthenticationClient returns the secret client to communicate with the wssdagent
+func GetAuthenticationClient(serverAddress *string, authorizer auth.Authorizer) (security_pb.AuthenticationAgentClient, error) {
+	opts := getDefaultDialOption(authorizer)
+	conn, err := grpc.Dial(getServerEndpoint(serverAddress), opts...)
+	if err != nil {
+		log.Fatalf("Unable to get AuthenticationClient. Failed to dial: %v", err)
+	}
+
+	return security_pb.NewAuthenticationAgentClient(conn), nil
 }
