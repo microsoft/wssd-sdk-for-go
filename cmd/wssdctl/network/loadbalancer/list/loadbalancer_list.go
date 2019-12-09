294198b33930221e26f4fbf248c86dc8044820ea
Pradip Dhara <pradipd@microsoft.com>
2019-12-09 14:14:12 -0800

    Filling in Load Balancer SDK calls.

--- cmd/wssdctl/network/loadbalancer/list/loadbalancer_list.go
+++ cmd/wssdctl/network/loadbalancer/list/loadbalancer_list.go
@@ -3,11 +3,23 @@
 package list
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/microsoft/wssd-sdk-for-go/pkg/auth"
+	"github.com/microsoft/wssd-sdk-for-go/pkg/config"
+	"github.com/microsoft/wssd-sdk-for-go/services/network/loadbalancer"
+
+	wssdcommon "github.com/microsoft/wssd-sdk-for-go/common"
 )
 
 type flags struct {
-	Name string
+	Name   string
+	Output string
+	Query  string
 }
 
 func NewCommand() *cobra.Command {
@@ -22,11 +34,39 @@ func NewCommand() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().StringVar(&flags.Output, "output", "yaml", "Output Format [yaml, json, csv, tsv]")
+	cmd.Flags().StringVar(&flags.Query, "query", "", "Output Format")
 	return cmd
 }
 
 func runE(flags *flags) error {
-	panic("lb list not implemented")
+	group := viper.GetString("group")
+	server := viper.GetString("server")
+
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		return err
+	}
+
+	lbclient, err := loadbalancer.NewLoadBalancerClient(server, authorizer)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), wssdcommon.DefaultServerContextTimeout)
+	defer cancel()
+
+	lbs, err := lbclient.Get(ctx, group, flags.Name)
+	if err != nil {
+		return err
+	}
+	if lbs == nil || len(*lbs) == 0 {
+		fmt.Println("No Load Balancer Resources")
+		// Not an error
+		return nil
+	}
+
+	config.PrintFormatList(*lbs, flags.Query, flags.Output)
 
 	return nil
 }
