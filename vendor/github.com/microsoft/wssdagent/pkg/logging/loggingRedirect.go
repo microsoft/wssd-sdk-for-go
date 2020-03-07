fad301d6d7143fa39e2e0d94612b6c3781760b2c
Nathan Gieseker <nagiesek@microsoft.com>
2020-03-06 16:47:57 -0800

    vendor update

---
+++ vendor/github.com/microsoft/wssdagent/pkg/logging/loggingRedirect.go
@@ -0,0 +1,83 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// Package loggingRedirect - Creates a log file the redirects STD output.
+package logging
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	path "github.com/microsoft/wssdagent/pkg/path"
+)
+
+var (
+	oldStdOut *os.File
+	oldStdErr *os.File
+	logFile   *os.File
+)
+
+func createLogFile(logFileAbsolutePath string, logFileName string) (*os.File, error) {
+	// Create log path
+	os.MkdirAll(logFileAbsolutePath, os.ModeDir)
+
+	err := path.CheckPath(logFileAbsolutePath)
+	if err != nil {
+		return nil, err
+	}
+	path := filepath.Join(logFileAbsolutePath, logFileName)
+	logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	st, err := logFile.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	// If there are contents in the file already, move the file and replace it.
+	if st.Size() > 0 {
+		logFile.Close()
+		os.Rename(path, path+".old")
+		logFile, err = os.Create(path)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return logFile, nil
+}
+
+// StartRedirectingOutput
+func StartRedirectingOutput(logFileAbsolutePath string, logFileName string) error {
+	// Create output file
+	var err error
+	logFile, err = createLogFile(logFileAbsolutePath, logFileName)
+	if err != nil {
+		return err
+	}
+
+	// Save previous values
+	oldStdOut = os.Stdout
+	oldStdErr = os.Stderr
+
+	// Set output to file
+	os.Stdout = logFile
+	os.Stderr = logFile
+	log.SetOutput(logFile)
+
+	return nil
+}
+
+// RestoreOutput
+func RestoreOutput() {
+	// Restoring previous values
+	os.Stdout = oldStdOut
+	os.Stderr = oldStdErr
+	log.SetOutput(os.Stderr)
+
+	// Close log file
+	logFile.Close()
+}
