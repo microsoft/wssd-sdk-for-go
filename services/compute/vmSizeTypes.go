7593550868431666f554d46e86b91c460ada16f5
Erik Frimodig <erfrimod@microsoft.com>
2020-03-11 14:33:55 -0700

    Adding supported size table

--- services/compute/vmSizeTypes.go
+++ services/compute/vmSizeTypes.go
@@ -35,6 +35,32 @@ type VirtualMachineSizeTypes string
 // For more information about virtual machine sizes, see 'Sizes for virtual machines':
 //  https://docs.microsoft.com/en-us/azure/virtual-machines/windows/sizes
 
+/*
+The following Size Types are supported:
+
+				CPU  GBRAM
+Default            4    4
+Standard_A1_v2     1    2
+Standard_A2_v2     2    4
+Standard_A4_v2     4    8
+-
+Standard_D2s_v3    2    8
+Standard_D4s_v3    4   16
+Standard_D8s_v3    8   32
+Standard_D16s_v3  16   64
+Standard_D32s_v3  32  128
+-
+Standard_DS2_v2    2    7
+Standard_DS3_v2    2   14
+Standard_DS4_v2    8   28
+Standard_DS5_v2   16   56
+-
+Standard_DS13_v2   8   56
+-
+Standard_K8S_v1    4    2 (custom for IoT)
+Standard_K8S2_v1   2    2 (custom for IoT)
+*/
+
 const (
 	// VirtualMachineSizeTypesDefault ...
 	VirtualMachineSizeTypesDefault VirtualMachineSizeTypes = "Default"
