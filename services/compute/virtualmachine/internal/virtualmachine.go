3d0b165e71a7e1089aa6be5fb09f13bdf26f44ad
Anthony Weston <aweston@microsoft.com>
2021-07-14 11:08:44 -0700

    winrm

--- services/compute/virtualmachine/internal/virtualmachine.go
+++ services/compute/virtualmachine/internal/virtualmachine.go
@@ -229,6 +229,22 @@ func (c *client) getWssdVirtualMachineWindowsConfiguration(windowsConfiguration 
 		RDPConfiguration: &wssdcompute.RDPConfiguration{},
 	}
 
+	if windowsConfiguration.WinRM != nil && windowsConfiguration.WinRM.Listeners != nil && len(*windowsConfiguration.WinRM.Listeners) >= 1 {
+		listeners := make([]*wssdcommonproto.WinRMListener, len(*windowsConfiguration.WinRM.Listeners))
+		for i, listener := range *windowsConfiguration.WinRM.Listeners {
+			protocol := wssdcommonproto.WinRMProtocolType_HTTP
+			if listener.Protocol == compute.HTTPS {
+				protocol = wssdcommonproto.WinRMProtocolType_HTTPS
+			}
+			listeners[i] = &wssdcommonproto.WinRMListener{
+				Protocol: protocol,
+			}
+		}
+		wc.WinRMConfiguration = &wssdcommonproto.WinRMConfiguration{
+			Listeners: listeners,
+		}
+	}
+
 	if windowsConfiguration == nil {
 		return wc
 	}
@@ -470,6 +486,22 @@ func (c *client) getVirtualMachineWindowsConfiguration(windowsConfiguration *wss
 
 	if windowsConfiguration == nil {
 		return wc
+	}
+
+	if windowsConfiguration.WinRMConfiguration != nil && len(windowsConfiguration.WinRMConfiguration.Listeners) >= 1 {
+		listeners := make([]compute.WinRMListener, len(windowsConfiguration.WinRMConfiguration.Listeners))
+		for i, listener := range windowsConfiguration.WinRMConfiguration.Listeners {
+			protocol := compute.HTTP
+			if listener.Protocol == wssdcommonproto.WinRMProtocolType_HTTPS {
+				protocol = compute.HTTPS
+			}
+			listeners[i] = compute.WinRMListener{
+				Protocol: protocol,
+			}
+		}
+		wc.WinRM = &compute.WinRMConfiguration{
+			Listeners: &listeners,
+		}
 	}
 
 	if windowsConfiguration.RDPConfiguration != nil {
