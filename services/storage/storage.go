d6d24a7729d6096d3238d20ba292828682f909ba
Harika Vedati <havedati@microsoft.com>
2022-03-22 10:28:26 -0700

    updating to latest realease of moc

--- services/storage/storage.go
+++ services/storage/storage.go
@@ -52,7 +52,7 @@ type VirtualHardDiskProperties struct {
 	CloudInitDataSource common.CloudInitDataSource `json:"cloudInitDataSource,omitempty"`
 	// HyperVGeneration - Gets the HyperVGenerationType of the VirtualMachine created from the image. Possible values include: 'HyperVGenerationTypesV1', 'HyperVGenerationTypesV2'
 	HyperVGeneration common.HyperVGeneration `json:"hyperVGeneration,omitempty"`
-	//DiskFileFormat - File format of the disk 
+	//DiskFileFormat - File format of the disk
 	DiskFileFormat common.DiskFileFormat `json:"diskFileFormat,omitempty"`
 }
 
