ed73b297be3c07c453e842e4321c56207c43f248
wenjlu-lgy <wenjlu@microsoft.com>
2024-06-21 14:21:12 +0800

    fix potential null reference issue

--- services/compute/virtualmachine/internal/virtualmachine.go
+++ services/compute/virtualmachine/internal/virtualmachine.go
@@ -115,23 +115,27 @@ func (c *client) getWssdVirtualMachineHardwareConfiguration(vm *compute.VirtualM
 		}
 		if vm.HardwareProfile.VirtualMachineGPUs != nil {
 			for _, gpu := range vm.HardwareProfile.VirtualMachineGPUs {
-				var assignment wssdcommonproto.AssignmentType
-				switch *gpu.Assignment {
-				case compute.GpuDDA:
-					assignment = wssdcommonproto.AssignmentType_GpuDDA
-				case compute.GpuP:
-					assignment = wssdcommonproto.AssignmentType_GpuP
-				case compute.GpuPV:
-					assignment = wssdcommonproto.AssignmentType_GpuPV
-				case compute.GpuDefault:
-					assignment = wssdcommonproto.AssignmentType_GpuDefault
+				if gpu != nil {
+					var assignment wssdcommonproto.AssignmentType
+					switch *gpu.Assignment {
+					case compute.GpuDDA:
+						assignment = wssdcommonproto.AssignmentType_GpuDDA
+					case compute.GpuP:
+						assignment = wssdcommonproto.AssignmentType_GpuP
+					case compute.GpuPV:
+						assignment = wssdcommonproto.AssignmentType_GpuPV
+					case compute.GpuDefault:
+						assignment = wssdcommonproto.AssignmentType_GpuDefault
+					}
+					vmGPU := &wssdcommonproto.VirtualMachineGPU{
+						Assignment:      assignment,
+						PartitionSizeMB: *gpu.PartitionSizeGB,
+						Name:            *gpu.Name,
+					}
+					vmGPUs = append(vmGPUs, vmGPU)
+				} else {
+					return nil, errors.Wrapf(errors.InvalidInput, "nil value in Hardware.VirtualMachineGPUs")
 				}
-				vmGPU := &wssdcommonproto.VirtualMachineGPU{
-					Assignment:      assignment,
-					PartitionSizeMB: *gpu.PartitionSizeGB,
-					Name:            *gpu.Name,
-				}
-				vmGPUs = append(vmGPUs, vmGPU)
 			}
 		}
 	}
