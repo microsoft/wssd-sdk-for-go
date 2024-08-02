3ae76f450f46b964f59b50c5aedea54b96e00a8c
Michael Duppré <mduppre@microsoft.com>
2024-08-02 15:24:08 -0700

    Add VHD hydration calls

--- services/storage/virtualharddisk/internal/wssd.go
+++ services/storage/virtualharddisk/internal/wssd.go
@@ -77,6 +77,18 @@ func (c *client) Delete(ctx context.Context, containerName, name string) error {
 	return err
 }
 
+// The interface for the hydrate call takes the container name and the name of the disk file.
+// Ultimately, we need the full path on disk to the disk file which we assemble from the path of the container plus the file name of the disk.
+// (e.g. "C:\ClusterStorage\Userdata_1\abc123" for the container path and "my_disk.vhd" for the disk name)
+func (c *client) Hydrate(ctx context.Context, containerName, name string) (*storage.VirtualHardDisk, error) {
+	request, err := getVirtualHardDiskRequest(wssdcommonproto.Operation_HYDRATE, name, containerName, nil)
+	if err != nil {
+		return nil, err
+	}
+	_, err = c.VirtualHardDiskAgentClient.Invoke(ctx, request)
+	return nil, err
+}
+
 func getVirtualHardDisksFromResponse(response *wssdstorage.VirtualHardDiskResponse) *[]storage.VirtualHardDisk {
 	virtualHardDisks := []storage.VirtualHardDisk{}
 	for _, vhd := range response.GetVirtualHardDiskSystems() {
